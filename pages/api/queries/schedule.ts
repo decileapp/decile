2d1205aa5e5007d3350d23c0b5204517f022d907
Krishna Nandakumar <krishnanandakumar@Krishnas-MacBook-Pro.local>
2022-09-30 01:04:06 +0100

    turned off scheduling

--- pages/api/queries/schedule.ts
+++ pages/api/queries/schedule.ts
@@ -15,52 +15,56 @@ export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.headers.bearer?.toString() !== process.env.BEARER_TOKEN) {
+  console.log(req.headers);
+  if (
+    !req.headers["bearer"] ||
+    req.headers["bearer"].toString() !== process.env.BEARER_TOKEN
+  ) {
     res.status(401).json({});
     return;
   } else if (req.method === "GET") {
     try {
-      const serviceSupabase = await getServiceSupabase();
-      const { data, error } = await serviceSupabase
-        .from("schedule")
-        .select("run_at, query_id, user_id, org_id");
-      if (!data) {
-        res.status(200).json({});
-        return;
-      }
+      //   const serviceSupabase = await getServiceSupabase();
+      //   const { data, error } = await serviceSupabase
+      //     .from("schedule")
+      //     .select("run_at, query_id, user_id, org_id");
+      //   if (!data) {
+      //     res.status(200).json({});
+      //     return;
+      //   }
 
-      if (data && data.length == 0) {
-        res.status(200).json({});
-        return;
-      }
+      //   if (data && data.length == 0) {
+      //     res.status(200).json({});
+      //     return;
+      //   }
 
-      // Run all queries
-      const runQueries = data.concat(data).map(async (singleQuery) => {
-        const data = await queryById({
-          queryId: singleQuery.query_id,
-          userId: singleQuery.user_id,
-          orgId: singleQuery.org_id,
-        });
-        // Export to GSheets
-        // Check if token exists
-        const auth = await checkExistingToken(singleQuery.user_id);
+      //   // Run all queries
+      //   const runQueries = data.map(async (singleQuery) => {
+      //     const data = await queryById({
+      //       queryId: singleQuery.query_id,
+      //       userId: singleQuery.user_id,
+      //       orgId: singleQuery.org_id,
+      //     });
+      //     // Export to GSheets
+      //     // Check if token exists
+      //     const auth = await checkExistingToken(singleQuery.user_id);
 
-        // If no auth redir
-        if (!auth) {
-          return;
-        } else {
-          const rowData = await formatForSheets(data);
-          const createdSheet = await createAndWriteSpreadsheet({
-            auth: auth,
-            title: `NewSample-${new Date(Date.now())}`,
-            range: "Sheet1",
-            data: rowData,
-          });
-          return createdSheet;
-        }
-      });
+      //     // If no auth redir
+      //     if (!auth) {
+      //       return;
+      //     } else {
+      //       const rowData = await formatForSheets(data);
+      //       const createdSheet = await createAndWriteSpreadsheet({
+      //         auth: auth,
+      //         title: `NewSample-${new Date(Date.now())}`,
+      //         range: "Sheet1",
+      //         data: rowData,
+      //       });
+      //       return createdSheet;
+      //     }
+      //   });
 
-      const runAllQueries = await Promise.all(runQueries);
+      //   const runAllQueries = await Promise.all(runQueries);
       res.status(200).json({});
       return;
     } catch (e: any) {
